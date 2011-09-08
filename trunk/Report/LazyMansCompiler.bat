4a402ae59a3539db960a6c7f863f05aa1b67d4e3
tbirch <tbirch@776575bb-2c05-bb74-d14f-5707e1cf37c2>
2011-09-08 10:18:30 +0000

    Added the basics

---
+++ trunk/Report/LazyMansCompiler.bat
@@ -0,0 +1,62 @@
+@echo off
+:: Closing Adobe reader.
+tasklist /FI "IMAGENAME eq acrord32.exe" | find /i "acrord32.exe" 
+ 
+IF ERRORLEVEL 2 GOTO EXIST
+IF ERRORLEVEL 1 GOTO NOTEXIST 
+ 
+:EXIST
+taskkill /F /IM "acrord32.exe"
+set reopenvar=yes
+goto FIRSTCLEANING
+ 
+:NOTEXIST
+set reopenvar=no
+goto FIRSTCLEANING
+
+
+:FIRSTCLEANING
+IF NOT EXIST master.aux goto COMPILING
+::Cleaning if last compile failed
+del master.aux
+del master.log
+del master.out
+del master.synctex.gz
+del master.toc
+del master.bbl
+del master.synctex.gz(busy)
+del master.blg
+GOTO COMPILING
+
+::ERROR
+:ERRORMASTER
+echo master.tex not found.
+goto ENDWITHPAUSE
+
+::Compiling
+:COMPILING
+IF NOT EXIST master.tex goto ERRORMASTER
+pdflatex.exe master.tex
+pdflatex.exe master.tex
+bibtex.exe master.aux
+pdflatex.exe master.tex
+pdflatex.exe master.tex
+del master.aux
+del master.log
+del master.out
+del master.synctex.gz
+del master.toc
+del master.bbl
+del master.synctex.gz(busy)
+del master.blg
+
+
+::Starting adobe reader again
+:START
+IF %reopenvar%==yes start acrord32.exe master.pdf
+goto END
+
+:ENDWITHPAUSE
+pause
+
+:END
