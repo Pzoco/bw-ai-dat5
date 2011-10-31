7098e5b702597470e98a86e58b3ef2e6ff638814
tbirch@gmail.com <tbirch@gmail.com@776575bb-2c05-bb74-d14f-5707e1cf37c2>
2011-10-31 09:15:37 +0000

    did GetNearestEnemy and unit's maximum shooting range for both ground and air, added template for GetSquadCenter.

--- code/scai/StarcraftAI/MathHelper.cpp
+++ code/scai/StarcraftAI/MathHelper.cpp
@@ -31,16 +31,50 @@ std::list<BWAPI::Position> MathHelper::GetSurroundingPositions(int x, int y, int
 	return positions;
 }
 
-void MathHelper::GetNearestEnemy(int x, int y)
+int MathHelper::GetNearestEnemy(int x, int y)
 {
-	std::set<BWAPI::Player*> enemies = BWAPI::Broodwar->enemies();
-	//BWAPI::Broodwar->enemy()
+	std::set<BWAPI::Player*> enemies = BWAPI::Broodwar->enemies();	
+	std::set<BWAPI::Unit*> enemieUnits;
+	int distance = 30000;
 
+	//Now to get all the enemies units in one list.
+	//Iterating trough all the enemies
+	for(std::set<BWAPI::Player*>::const_iterator i = enemies.begin(); i != enemies.end(); i++)
+	{
+		//Making a list of that players units
+		std::set<BWAPI::Unit*> tempUnits = (*i)->getUnits();
+
+		//Iterating trough them.
+		for(std::set<BWAPI::Unit*>::iterator j = tempUnits.begin(); j != tempUnits.end(); j++)
+		{
+			//And add them to a list of all enemieunits in the map.
+			enemieUnits.insert((*j));
+		}
+	}
+
+	//If there is any enemie units we know of.
+	if(enemieUnits.empty() != true)
+	{
+		//Calculating the distance to the nearest one,
+		for(std::set<BWAPI::Unit*>::iterator k = enemieUnits.begin(); k != enemieUnits.end(); k++)
+		{
+			int getDist = (*k)->getDistance(BWAPI::Position(x,y));
+			if(getDist < distance)
+			{
+				distance = getDist;
+			}
+		}
+	}
+
+	return distance;
 }
 int MathHelper::GetNearestAlly(int x, int y)
 {
+	//Getting a list of all our units.
 	std::set<BWAPI::Unit*> myUnits = BWAPI::Broodwar->self()->getUnits();
 	int distance = 30000;
+
+	//Iterating trough them, and calculating the distance to the closest one
 	for(std::set<BWAPI::Unit*>::const_iterator i = myUnits.begin(); i != myUnits.end(); i++)
 	{
 		int getDist = (*i)->getDistance(BWAPI::Position(x,y));
@@ -50,4 +84,10 @@ int MathHelper::GetNearestAlly(int x, int y)
 		}
 	}
 	return distance;
+}
+//This function takes int &x, int &y not to use it as input but to allow to return to values ;)
+void MathHelper::GetSquadCenter(std::list<BWAPI::Unit*>, int &x, int &y)
+{
+	
+	return;
 }
